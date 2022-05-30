769a3639d152aa7ab98f60a9450c914aa34716eb
Danmalkaa <45342726+Danmalkaa@users.noreply.github.com>
2022-05-30 12:20:57 +0300

    Update models.py

--- models/models.py
+++ models/models.py
@@ -60,22 +60,22 @@ class EmbeddingImagenet(nn.Module):
         self.args = args
 
         # Input 84x84x3
-        self.conv1 = nn.Conv2d(3, self.ndf, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(3, self.ndf, kernel_size=3, stride=1, padding=1, bias=True)
         self.bn1 = nn.BatchNorm2d(self.ndf)
 
         # Input 42x42x64
-        self.conv2 = nn.Conv2d(self.ndf, int(self.ndf*1.5), kernel_size=3, bias=False)
+        self.conv2 = nn.Conv2d(self.ndf, int(self.ndf*1.5), kernel_size=3, bias=True)
         self.bn2 = nn.BatchNorm2d(int(self.ndf*1.5))
 
         # Input 20x20x96
-        self.conv3 = nn.Conv2d(int(self.ndf*1.5), self.ndf*2, kernel_size=3, padding=1, bias=False)
+        self.conv3 = nn.Conv2d(int(self.ndf*1.5), self.ndf*2, kernel_size=3, padding=1, bias=True)
         self.bn3 = nn.BatchNorm2d(self.ndf*2)
-        self.drop_3 = nn.Dropout2d(0.4)
+        self.drop_3 = nn.Dropout2d(0.1)
 
         # Input 10x10x128
-        self.conv4 = nn.Conv2d(self.ndf*2, self.ndf*4, kernel_size=3, padding=1, bias=False)
+        self.conv4 = nn.Conv2d(self.ndf*2, self.ndf*4, kernel_size=3, padding=1, bias=True)
         self.bn4 = nn.BatchNorm2d(self.ndf*4)
-        self.drop_4 = nn.Dropout2d(0.5)
+        self.drop_4 = nn.Dropout2d(0.1)
 
         # Input 5x5x256
         self.fc1 = nn.Linear(self.ndf*4*5*5, self.emb_size, bias=True)
